7473c9295f58a79fafd151937efd2985936596ed
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2020-12-03 23:06:49 +1100

    add input of forces and material properties in sidebar, calculate longit stif spacing

--- functions.py
+++ functions.py
@@ -12,3 +12,11 @@ from sectionproperties.analysis.cross_section import CrossSection
 #Import other py files
 import section_funcs
 
+#Import handcalcs
+from handcalcs import handcalc
+
+@handcalc(override="long")
+def longit_stif_spacing(b, d, n_stif):
+    b_flange = b / (n_stif + 1) #spacing of longitudinal stiffeners in flange
+    b_web = d / (n_stif + 1) #spacing of longitudinal stiffeners in web
+    return b_flange, b_web
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 import forallpeople as u
 u.environment('structural')
 
+from handcalcs import handcalc
+
 #Main function calls made at each run of Streamlit
 def main():
     """This function is run at the beginning of each script"""
@@ -38,7 +40,7 @@ def main():
     - **7.4.3** - Buckling of Web Panels
     - **7.4.4** - Longitudinal Web Stiffeners
     - **7.4.6** - Transverse Stiffeners of Longitudinally Stiffened Webs
-        - **5.10** - Stiffened Web $\alpha_v$ calculation
+        - **5.10** - Stiffened Web alpha_v calculation
         - **5.14** - Design of Intermediate Transverse Web Stiffeners
         
         
@@ -79,8 +81,35 @@ def main():
     t_stif = st.sidebar.slider('Thickness longit. stiffeners (mm)',5,30,12,step=1,format='%i') / 1000
     d_stif_trans = st.sidebar.slider('Depth/Height tranv. stiffeners / diaphragm (mm)',50,300,100,step=5,format='%i') / 1000
     t_stif_trans = st.sidebar.slider('Thickness of transv. stiffeners / diaphragm(mm)',5,30,12,step=1,format='%i') / 1000
-    s_stif_trans = st.sidebar.slider('Spacing of transverse stiffeners (mm)',300,3000,1000,step=50,format='%i') / 1000
+    a_panel = st.sidebar.slider('Spacing of transverse stiffeners (mm)',300,3000,1000,step=50,format='%i') / 1000
 
-    st.write(f"n_stif = {n_stif}")
+    #Calculate stiffener dimensions
+    longit_stif_spacing_latex, longit_stif_spacing_vals = fnc.longit_stif_spacing(b, d, n_stif)
+    st.latex(longit_stif_spacing_latex)
+    b_flange,b_web = longit_stif_spacing_vals
+
+    #Input Materials
+    st.sidebar.markdown("## Input Material Properties")
+    f_y = st.sidebar.number_input("Yield Strength (MPa)",150,500,350,10,"%i") * 10**6
+    E_s = st.sidebar.number_input("Young's Modulus (GPa)",40,300,200,5,"%i") * 10**9
+
+    phi = 0.9 #T3.2 (b) (i) - Members subject to bending Cl7.3
+    rho_steel = 7850 #Density of steel
+    g = 9.81 #Gravity
+
+    st.sidebar.latex(r"\phi={0.9}")
+    st.sidebar.latex(r"\rho_{steel}=7850 \frac{kg}{m^3}")
+    st.sidebar.latex(r"g=9.81 \frac{N}{kg}")
+
+    #Force values
+    st.sidebar.markdown("## Input Forces")
+    Fx = st.sidebar.number_input("F_x Axial Force typically due to thermal or restraint effects (kN)",0,1000,100,10,"%i") * 1000
+    Fy = st.sidebar.number_input("F_y Vertical Shear Force (kN)",0,5000,100,10,"%i") * 1000
+    Fz = st.sidebar.number_input("F_z Horizontal Shear Force (kN)",0,5000,100,10,"%i") * 1000
+    Mx = st.sidebar.number_input("Mx Torsion Force (kNm)",0,5000,100,10,"%i")
+    My = st.sidebar.number_input("My Transverse BM (kNm)",0,20000,1000,10,"%i")
+    Mz = st.sidebar.number_input("Mz Vertical BM (kNm)",0,20000,1000,10,"%i") * 1000
+    rho = st.sidebar.slider("Proportion of longitudinal stress assumed to be redistributed from web to flange (%)",0,100,0,5,"%i") / 100
+
 if __name__ == '__main__':
     main()
